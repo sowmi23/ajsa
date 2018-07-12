4da6f78667ecf4f786d215f0c2491071c5e18ac6
sowmi23 <41141676+sowmi23@users.noreply.github.com>
2018-07-12 14:53:02 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,6 @@
 var=input()
 alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 if var in alph:
-	print("yes")
+	print("Alphabet")
 else:
 	print("No")
